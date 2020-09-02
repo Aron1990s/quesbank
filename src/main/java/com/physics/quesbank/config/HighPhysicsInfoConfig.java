75812addaaa09e347db06b3f2fbf81f2a9ed4fc8
zch1990s <zch1990s@gmail.com>
2020-09-02 17:30:00 +0800

    xx

--- src/main/java/com/physics/quesbank/config/HighPhysicsInfoConfig.java
+++ src/main/java/com/physics/quesbank/config/HighPhysicsInfoConfig.java
@@ -1,7 +1,6 @@
 package com.physics.quesbank.config;
 
-import com.physics.quesbank.entity.HighPhysicsInfo.HighGradeInfo;
-import com.physics.quesbank.entity.HighPhysicsInfo.HighPhysicsInfo;
+import com.physics.quesbank.entity.HighPhysicsInfo.*;
 import com.physics.quesbank.service.highPhysicsInfo.HighPhysicsInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,12 +29,28 @@ public class HighPhysicsInfoConfig {
     @Bean
     public HighPhysicsInfo initHighPhysicsInfoConfig(){
         HighPhysicsInfo highPhysicsInfo = new HighPhysicsInfo();
+        Map<String, List<HighChapterInfo>> chapters = new HashMap<>();
+        Map<String, List<HighChapterSubInfo>> chapterSubs = new HashMap<>();
+        Map<String, List<HighChapterSubItemInfo>> chapterSubItems = new HashMap<>();
+        logger.info("saasssss");
         try{
             List<HighGradeInfo> highGradeInfos = highPhysicsInfoService.listHighGradeInfo();
             highPhysicsInfo.setGrades(highGradeInfos);
             for (HighGradeInfo highGradeInfo: highGradeInfos) {
-                
+                List<HighChapterInfo> highChapterInfos = highPhysicsInfoService.listHighChapterInfo(highGradeInfo);
+                chapters.put(highGradeInfo.getId()+"", highChapterInfos);
+                for (HighChapterInfo highChapterInfo:highChapterInfos) {
+                    List<HighChapterSubInfo> highChapterSubInfos = highPhysicsInfoService.listHighChapterSubInfo(highChapterInfo);
+                    chapterSubs.put(highChapterInfo.getGrade_id()+"", highChapterSubInfos);
+                    for (HighChapterSubInfo highChapterSubInfo:highChapterSubInfos) {
+                        List<HighChapterSubItemInfo> highChapterSubItemInfos = highPhysicsInfoService.listHighChapterSubItemInfo(highChapterSubInfo);
+                        chapterSubItems.put(highChapterSubInfo.getChapter_id()+"", highChapterSubItemInfos);
+                    }
+                }
             }
+            highPhysicsInfo.setChapters(chapters);
+            highPhysicsInfo.setChapterSubs(chapterSubs);
+            highPhysicsInfo.setChapterSubItems(chapterSubItems);
         } catch (Exception e){
             e.printStackTrace();
         }
