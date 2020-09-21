108d135985f780d32c0043c5c8995819e0f1ec36
zch1990s <zch1990s@gmail.com>
2020-09-21 14:25:16 +0800

    xx

--- src/main/java/com/physics/quesbank/controller/base/BaseController.java
+++ src/main/java/com/physics/quesbank/controller/base/BaseController.java
@@ -1,5 +1,8 @@
 package com.physics.quesbank.controller.base;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,9 +18,21 @@ public class BaseController {
 
     protected final static Logger logger = LoggerFactory.getLogger(BaseController.class);
 
+    /**
+     * 获取UUID
+     * @return
+     */
     public static String getUUID(){
         return UUID.randomUUID().toString().replace("-", "");
     }
 
-
+    /**
+     * 获取session
+     * @return
+     */
+    public static Session getSession(){
+        Subject currentUser = SecurityUtils.getSubject();
+        Session session = currentUser.getSession();
+        return session;
+    }
 }
