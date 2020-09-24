e3eb76417e3d989b6c140f54bc407caff40d1aa5
zch1990s <zch1990s@gmail.com>
2020-09-24 15:00:53 +0800

    xx

--- src/main/java/com/physics/quesbank/QuesbankApplication.java
+++ src/main/java/com/physics/quesbank/QuesbankApplication.java
@@ -1,5 +1,7 @@
 package com.physics.quesbank;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +10,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class QuesbankApplication {
 
+    protected final static Logger logger = LoggerFactory.getLogger(QuesbankApplication.class);
+
+
     public static void main(String[] args) {
         SpringApplication.run(QuesbankApplication.class, args);
+        logger.info("~~~乾杯~~梦想已在路上~~ (^_^)／★＼(^_^) ~~道路已在脚下~~乾杯~~~");
+
     }
 
 }
