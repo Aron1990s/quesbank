a7e425325e3a332e4cc145c31ada7b3c9d912bff
zch1990s <zch1990s@gmail.com>
2020-12-18 15:18:54 +0800

    xx

--- src/main/java/com/physics/quesbank/controller/highPhysicsQuestion/HighPhysicsQuestionController.java
+++ src/main/java/com/physics/quesbank/controller/highPhysicsQuestion/HighPhysicsQuestionController.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.InvalidMimeTypeException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -205,6 +206,8 @@ public class HighPhysicsQuestionController extends BaseController {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InvalidMimeTypeException e){
+            logger.info("InvalidMimeTypeException"+DateUtil.getTime());
         } catch (Exception e) {
             e.printStackTrace();
         }
